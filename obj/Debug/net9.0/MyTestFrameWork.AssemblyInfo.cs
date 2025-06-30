ba44ea3fceb5aa838a9fa5558ab30df82356a5c8
Anthony Grant <agrant@affinitiv.com>
2025-06-29 23:27:14 -0500

    I have to save to switch branches

--- obj/Debug/net9.0/MyTestFrameWork.AssemblyInfo.cs
+++ obj/Debug/net9.0/MyTestFrameWork.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyTestFrameWork")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5bda6e55e5eb4e988a8f74b96a8b02aee544c1aa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea909e3ec433da4e25285d2aec2fc3573334bfed")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyTestFrameWork")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyTestFrameWork")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
